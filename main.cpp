fa3d29ad6acfe1d77bee41f4aae6769c978ebfca
Ritik <kooltyme1@gmail.com>
2024-06-19 22:54:16 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,65 +1,60 @@
-/*
-#include <iostream>
-#include <windows.h>
-#include <string>
-
-int main() {
-
-    int count = 0;
-
-    while(true){
-
-
-
-
-        if(GetKeyState('A') & 0x8000)
-        {
-
-            count++;
-            std::cout << "pressed" + std::to_string(count) << std::endl;
-
-        }
-
-    }
-
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
-*/
 
 #include <windows.h>
 #include <fstream>
 #include <string>
 #include <iostream>
 
-char keyToHook = 'Z';
+#include <iomanip>
+#include <ctime>
+#include <conio.h>
+
+#include <thread>
+
+char keyToHook = 'Q';
+char keyToSend = 'W';
+int sleepTime = 10;
+
+
+
+
 
 // The hook procedure
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+
+    static bool ignore = false;
+
     if (nCode == HC_ACTION) {
         // Get the key information
         KBDLLHOOKSTRUCT* keyStruct = (KBDLLHOOKSTRUCT*)lParam;
 
         // Check for a specific key (e.g., 'A')
         if (keyStruct->vkCode == keyToHook) {
-            if ((wParam == WM_KEYUP || wParam == WM_SYSKEYUP) && ((keyStruct->flags & LLKHF_UP) != 0)) {
-                // The 'A' key was released, simulate an 'A' key press
 
-                // Key down event
+            if ((wParam == WM_KEYUP || wParam == WM_SYSKEYUP) && ((keyStruct->flags & LLKHF_UP) != 0)) {
+
                 INPUT input = { 0 };
                 input.type = INPUT_KEYBOARD;
-                input.ki.wVk = keyToHook;
+                input.ki.wVk = keyToSend;
                 SendInput(1, &input, sizeof(INPUT));
 
-                // Key up event
+                clock_t delta = (CLOCKS_PER_SEC * sleepTime) / 1000;
+                clock_t now = clock();
+                clock_t end = now + delta;
+                do {
+                    Sleep(1); //1ms
+                    now = clock();
+                } while((now < end) && !kbhit());
+
                 ZeroMemory(&input, sizeof(INPUT));
                 input.type = INPUT_KEYBOARD;
-                input.ki.wVk = keyToHook;
+                input.ki.wVk = keyToSend;
                 input.ki.dwFlags = KEYEVENTF_KEYUP;
                 SendInput(1, &input, sizeof(INPUT));
 
-                // Cancel the original key release
+                ignore = false;
+
                 return 1;
+
             }
             else{
                 return 1;
@@ -72,25 +67,42 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 }
 
 int main() {
-    // Set the hook
+
 
     // Read the key from the config file
     std::ifstream configFile("config.txt");
-    if (configFile.is_open()) {
-        std::string line;
-        if (std::getline(configFile, line)) {
-            size_t startPos = line.find('\'');
-            size_t endPos = line.rfind('\'');
-            if (startPos != std::string::npos && endPos != std::string::npos && startPos < endPos) {
-                keyToHook = line[startPos + 1];
-            }
+    std::string line;
+
+    if (std::getline(configFile, line)) {
+        size_t startPos = line.find('\'');
+        size_t endPos = line.rfind('\'');
+        if (startPos != std::string::npos && endPos != std::string::npos && startPos < endPos) {
+            keyToHook = line[startPos + 1];
         }
-        configFile.close();
     }
 
-    HHOOK keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+    std::cout << "Hacked Key: " << keyToHook << std::endl;
 
-    std::cout << "Key to upstroke: " + keyToHook << std::endl;
+    if (std::getline(configFile, line)) {
+        size_t startPos = line.find('\'');
+        size_t endPos = line.rfind('\'');
+        if (startPos != std::string::npos && endPos != std::string::npos && startPos < endPos) {
+            keyToSend = line[startPos + 1];
+        }
+    }
+
+    std::cout << "Key To Send: " << keyToSend << std::endl;
+
+    if (std::getline(configFile, line)) {
+
+        sleepTime = std::stoi(line.substr(line.find("=")+1, line.length()));
+    }
+
+    configFile.close();
+
+    std::cout << "Press Length: " << sleepTime << std::endl;
+
+    HHOOK keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
 
     // Main message loop
     MSG msg;
